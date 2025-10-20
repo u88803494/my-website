4fdf1dffc43bea83c3a2ba000214313bcb42381a
Henry <u88803494@gmail.com>
2025-10-20 17:24:31 +0800

    chore: remove not-found package
    Remove not-found package as 404 pages are app-specific and should use Next.js built-in not-found.tsx convention rather than being reusable packages.

--- packages/not-found/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Main feature export
-export { default as NotFoundFeature } from './NotFoundFeature';
-
-// Component exports
-export { default as ContactSection } from './components/ContactSection';
-export { default as NotFoundHero } from './components/NotFoundHero';
-export { default as QuickNavigation } from './components/QuickNavigation';
-export { default as SearchSuggestion } from './components/SearchSuggestion';
